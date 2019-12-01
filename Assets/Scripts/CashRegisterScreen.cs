c918493793e9814f4490a8414eb9babc92763844
Pablo Martínez Arango <martinezarangopablo@gmail.com>
2019-12-01 16:23:12 +0100

    Added script in order to allow users to start and end a coctel and to show the points obtained

---
+++ Assets/Scripts/CashRegisterScreen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CashRegisterScreen : MonoBehaviour {
+
+    public GameObject button;
+    public GameObject text;
+    private List<string> coctels;
+    private int points = 0;
+
+    private void Start()
+    {
+        button = GameObject.FindWithTag("Button");
+        text = GameObject.FindWithTag("Text");
+        coctels = new List<string>{"Mojito", "Caipirinha", "Pisco Sour"};
+    }
+
+    void StartGame()
+    {
+        SelectCoctel();
+        button.GetComponentInChildren<Text>().text = "Finish Coctel";
+    }
+
+    void EndGame()
+    {
+        AddScore();
+        button.GetComponentInChildren<Text>().text = "Start Coctel";
+    }
+
+    void SelectCoctel()
+    {
+        int rnd = new System.Random().Next(coctels.Count());
+        text.GetComponentInChildren<Text>().text = coctels[rnd];
+    }
+
+    void AddScore()
+    {
+        points += 10; // temporal
+        text.GetComponentInChildren<Text>().text = points + " $";
+    }
+
+}
